2cc198440bbb06597f6b7ca5248c2e7dcb3c5db4
Elif Efe <967elif541@gmail.com>
2025-03-17 19:59:18 +0300

    dependency

--- DependencyInjection/main.py
+++ DependencyInjection/main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI,Depends
+from typing import Annotated
 
 app=FastAPI()
+
+def hello_world():
+    return "Hello,welcome fastApi"
+
+HelloDependency=Annotated[str,Depends(hello_world)]
+
+def get_hello_world(hello: HelloDependency):
+    return f"Hello world service: {hello}"
+
+@app.get("/hello")
+def hello(message:str=Depends(get_hello_world)):
+    return {"message": message}
